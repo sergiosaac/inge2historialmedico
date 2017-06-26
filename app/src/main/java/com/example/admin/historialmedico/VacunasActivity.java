ab64e48bd4340f45f29ab4f8e0b0656355fe33c0
Sergio Amarilla <sergio.amarilla.cinco@gmail.com>
2017-06-26 10:17:41 -0400

    Portada y consumo de servicios

--- app/src/main/java/com/example/admin/historialmedico/VacunasActivity.java
+++ app/src/main/java/com/example/admin/historialmedico/VacunasActivity.java
@@ -1,12 +1,10 @@
 package com.example.admin.historialmedico;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -48,164 +46,7 @@ public class VacunasActivity extends AppCompatActivity {
         new VacunasActivity.obtenerVacunas().execute();
 
         this.idHijo = getIntent().getExtras().getString("idHijo");
-
-        Button boton = (Button) findViewById(R.id.abc);
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String host = "http://10.9.100.164:8080";
-
-                HttpHandler nuevo = new HttpHandler();
-
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put("idHijo", VacunasActivity.this.idHijo);
-                    obj.put("order", "nombre");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                String jsonStr = nuevo.sendHTTPData(host+"/WebApplication3/webresources/vacunas/obtenerVacunasPost/",obj);
-
-                Log.e(TAG, "Response from url: " + jsonStr);
-
-                if (jsonStr != null) {
-                    try {
-                        JSONArray jsonObj = new JSONArray(jsonStr);
-
-                        for (int i = 0; i < jsonObj.length(); i++) {
-                            JSONObject c = jsonObj.getJSONObject(i);
-
-                            String name = c.getString("nombre");
-                            String email = c.getString("fecha_aplicacion");
-                            String address = c.getString("aplicada");
-
-                            HashMap<String, String> contact = new HashMap<>();
-
-                            contact.put("name", name);
-                            contact.put("email", email);
-                            contact.put("mobile", "Inyectada: "+address);
-
-                            contactList.add(contact);
-                        }
-                    } catch (final JSONException e) {
-                        Log.e(TAG, "Json parsing error: " + e.getMessage());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(),
-                                        "Json parsing error: " + e.getMessage(),
-                                        Toast.LENGTH_LONG)
-                                        .show();
-                            }
-                        });
-
-                    }
-                } else {
-                    Log.e(TAG, "Couldn't get json from server.");
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "No se encontraron vacunas",
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-
-                }
-
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);
-
-                Toast.makeText(getApplicationContext(),
-                        "Ordenado alfabeticamente",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
-
-        Button fecha = (Button) findViewById(R.id.fecha);
-        fecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String host = "http://10.9.100.164:8080";
-
-                HttpHandler nuevo = new HttpHandler();
-
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put("idHijo", VacunasActivity.this.idHijo);
-                    obj.put("order", "fecha_aplicacion");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                String jsonStr = nuevo.sendHTTPData(host+"/WebApplication3/webresources/vacunas/obtenerVacunasPost/",obj);
-
-                Log.e(TAG, "Response from url: " + jsonStr);
-
-                if (jsonStr != null) {
-                    try {
-                        JSONArray jsonObj = new JSONArray(jsonStr);
-
-                        for (int i = 0; i < jsonObj.length(); i++) {
-                            JSONObject c = jsonObj.getJSONObject(i);
-
-                            String name = c.getString("nombre");
-                            String email = c.getString("fecha_aplicacion");
-                            String address = c.getString("aplicada");
-
-                            HashMap<String, String> contact = new HashMap<>();
-
-                            contact.put("name", name);
-                            contact.put("email", email);
-                            contact.put("mobile", "Inyectada: "+address);
-
-                            contactList.add(contact);
-                        }
-                    } catch (final JSONException e) {
-                        Log.e(TAG, "Json parsing error: " + e.getMessage());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(),
-                                        "Json parsing error: " + e.getMessage(),
-                                        Toast.LENGTH_LONG)
-                                        .show();
-                            }
-                        });
-
-                    }
-                } else {
-                    Log.e(TAG, "Couldn't get json from server.");
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "No se encontraron vacunas",
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-
-                }
-
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);
-
-                Toast.makeText(getApplicationContext(),
-                        "Ordenado por fecha",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
+        
 
     }
 
